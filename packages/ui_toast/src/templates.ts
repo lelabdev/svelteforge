0a3a3bd7da32df6dd09bd6fe9f3f688b877e587d
Ludo Loops <ludoloops@pm.me>
2026-07-13 00:05:42 +0200

    chore(templates): regenerate deterministic manifests

--- packages/ui_toast/src/templates.ts
+++ packages/ui_toast/src/templates.ts
@@ -1,5 +1,5 @@
 // AUTO-GENERATED - DO NOT EDIT
 export const files = {
-  "/src/lib/components/svforge/ui/Toaster.svelte": "<script lang=\"ts\">\n\timport { Toast } from '@skeletonlabs/skeleton-svelte';\n\timport { toaster } from './toaster.ts';\n</script>\n\n<Toast.Group {toaster}>\n\t{#snippet children(toast)}\n\t\t<Toast {toast}>\n\t\t\t<Toast.Message>\n\t\t\t\t<Toast.Title>{toast.title}</Toast.Title>\n\t\t\t\t{#if toast.description}\n\t\t\t\t\t<Toast.Description>{toast.description}</Toast.Description>\n\t\t\t\t{/if}\n\t\t\t</Toast.Message>\n\t\t\t<Toast.CloseTrigger />\n\t\t</Toast>\n\t{/snippet}\n</Toast.Group>\n",
-  "/src/lib/components/svforge/ui/toaster.ts": "import { createToaster } from '@skeletonlabs/skeleton-svelte';\n\nexport const toaster = createToaster();\n"
+  "/lib/components/svforge/ui/Toaster.svelte": "<script lang=\"ts\">\n\timport { Toast } from '@skeletonlabs/skeleton-svelte';\n\timport { toaster } from './toaster.ts';\n</script>\n\n<Toast.Group {toaster}>\n\t{#snippet children(toast)}\n\t\t<Toast {toast}>\n\t\t\t<Toast.Message>\n\t\t\t\t<Toast.Title>{toast.title}</Toast.Title>\n\t\t\t\t{#if toast.description}\n\t\t\t\t\t<Toast.Description>{toast.description}</Toast.Description>\n\t\t\t\t{/if}\n\t\t\t</Toast.Message>\n\t\t\t<Toast.CloseTrigger />\n\t\t</Toast>\n\t{/snippet}\n</Toast.Group>\n",
+  "/lib/components/svforge/ui/toaster.ts": "import { createToaster } from '@skeletonlabs/skeleton-svelte';\n\nexport const toaster = createToaster();\n"
 };
